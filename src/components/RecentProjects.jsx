b6239f9091701990cf9472c33ca3ee03a2542138
sudharshansudhir <sudhir.ssr7@gmail.com>
2025-11-16 17:16:05 +0530

    Flixplore-AI project added

--- src/components/RecentProjects.jsx
+++ src/components/RecentProjects.jsx
@@ -28,7 +28,7 @@ const RecentProjects = () => {
     const projects=[
                  {
                       title:"Flixplore-AI",
-                      description:"A Movie streaming website with movie filtering and sorting Bot named 'DUDE'",
+                      description:"A Movie streaming website with movie filtering and sorting Bot named 'DUDE'.",
                       stack:"MERN STACK",
                       imageUrl:flixplore,
                       projectUrl:"https://flixplore-ai.vercel.app/",
